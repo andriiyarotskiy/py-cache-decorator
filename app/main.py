ee20c383e3a88b010428056e22876e8a106414b5
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2025-12-02 16:01:03 +0000

    Created cache decorator using dict

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    args_set = {}
+
+    def decorator(*args) -> Callable:
+        for key, arg in args_set.items():
+            if key == args:
+                print("Getting from cache")
+                return arg
+        print("Calculating new result")
+        result_of_operation = func(*args)
+        args_set[args] = result_of_operation
+        return result_of_operation
+
+    return decorator
